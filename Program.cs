f4ab3342113644a455f8cd674c958030d40b22a9
TurMikOmT <118839643+TurMikOmT@users.noreply.github.com>
2022-12-08 16:54:16 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//TASK-5                                                  08.12.2022
+
+//int age = 22;
+
+//if (age > 16)
+//{
+//    Console.WriteLine("sv ala biler");
+//}
+//else
+//{
+//    Console.WriteLine("sv ala bilmez");
+//}
+
+//TASK-2
+
+int i = 0;
+int number = 10;
+for (i = 0; i < 10; i++)
+{
+    number += i;
+}
+
+Console.WriteLine(number);
